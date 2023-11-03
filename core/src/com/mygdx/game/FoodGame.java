19606473de3a224acc17a15e7fa7378d6d6ca72c
mccarronr <122781875+mccarronr@users.noreply.github.com>
2023-11-03 15:17:24 +0100

    init
    Initial commit

---
+++ core/src/com/mygdx/game/FoodGame.java
@@ -0,0 +1,13 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+public class FoodGame extends ApplicationAdapter {
+	
+	@Override
+	public void create () {
+		//code goes here
+	}
+
+
+}
---
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.mygdx.game.FoodGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("Group Project Game");
+		new Lwjgl3Application(new FoodGame(), config);
+	}
+}
