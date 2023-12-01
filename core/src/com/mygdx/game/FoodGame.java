f202c800a16bae4938ecc238f6e3147c8e0b1a07
mccarronr <122781875+mccarronr@users.noreply.github.com>
2023-12-01 18:46:56 +0100

    Updated FoodGame so that it renders the Room

--- core/src/com/mygdx/game/FoodGame.java
+++ core/src/com/mygdx/game/FoodGame.java
@@ -1,12 +1,37 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Gdx;
+import com.mygdx.game.physics.Room;
 
-public class FoodGame extends ApplicationAdapter {
+public class FoodGame extends ApplicationAdapter 
+{
+	private Room kitchen1;
+	private SpriteBatch batch;
+	
 	
 	@Override
-	public void create () {
-		//code goes here
+	public void create () 
+	{
+		kitchen1 = new Room(1, false, 0);
+		batch = new SpriteBatch();
+	}
+	@Override
+	public void render ()
+	{
+		batch.begin();
+		ScreenUtils.clear(0, 0, 0, 0);
+		kitchen1.render(batch);
+		batch.end();
+
+	}
+	@Override
+	public void dispose()
+	{
+		batch.dispose();
 	}
 
 
