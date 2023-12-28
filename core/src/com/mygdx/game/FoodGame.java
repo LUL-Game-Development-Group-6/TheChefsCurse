25cc58a6d0574264e5755e3b5e378a731c0728c4
ginesmoratalla <ginesmoratalla@gmail.com>
2023-12-28 11:49:09 +0100

    GD-82-player-animated

--- core/src/com/mygdx/game/FoodGame.java
+++ core/src/com/mygdx/game/FoodGame.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.Gdx;
@@ -20,55 +21,99 @@ public class FoodGame extends ApplicationAdapter
 	private Room kitchen1;
 	private SpriteBatch batch;
 	private Player player1;
-	private ShapeRenderer shapeRenderer;//remove this later
+	private ShapeRenderer shapeRenderer;									//remove this later
 	private float playerSize;
+	private float timePassed;
 	
 	@Override
 	public void create () 
 	{
-		shapeRenderer = new ShapeRenderer();//remove this later
+		shapeRenderer = new ShapeRenderer();								//remove this later
 		kitchen1 = new Room(Room.RoomType.KITCHEN_DEMO, false, 0);
 		batch = new SpriteBatch();
-		player1 = new Player(400, 400, 56, 185);//first two are position. second two are for size of the hitbox
+		player1 = new Player(400, 400, 56, 185);			//first two are position. second two are for size of the hitbox
+		player1.create();
 		playerSize = 200;
 		
 	}
 	
-	
-	
 	@Override
 	public void render ()
 	{
+		batch.begin();
+		ScreenUtils.clear(0, 0, 0, 0);
+		timePassed += Gdx.graphics.getDeltaTime();
+		kitchen1.render(batch);
+		drawPlayer();
+		batch.end();
+	}
+	
+	@Override
+	public void dispose()
+	{
+		batch.dispose();
+		player1.dispose();
+	}
+
+	public void drawPlayer() {
+
 		float previousX = player1.getSprite().getX();
 		float previousY = player1.getSprite().getY();
-		
+		boolean isKeyPressed = false;
 		float speed = 5;
-		
-		
-		
-		
-		
-		
+
 		if (Gdx.input.isKeyPressed(Input.Keys.A)){
 			player1.testMove(-speed, 0);
 			if (player1.getFace()){
+				player1.flipAnimation();
 				player1.getSprite().flip(true, false);
 				player1.setFace(false);
 			}
+			isKeyPressed = true;
+
 		}
 		if (Gdx.input.isKeyPressed(Input.Keys.D)){
 			player1.testMove(speed, 0);
 			if (!player1.getFace()){
+				player1.flipAnimation();
 				player1.getSprite().flip(true, false);
 				player1.setFace(true);
 			}
+			isKeyPressed = true;
 		}
-		if (Gdx.input.isKeyPressed(Input.Keys.W)){
+
+		/*
+		 * Had to add condition to not be able to press W and S at the same time
+		 * to avoid overlapping the animations
+		 */
+		if (Gdx.input.isKeyPressed(Input.Keys.W) && !Gdx.input.isKeyPressed(Input.Keys.S)){
 			player1.testMove(0, speed);
+			batch.draw(player1.getAnimation().getKeyFrame(timePassed, true),
+			player1.getSprite().getX(), player1.getSprite().getY(),
+			playerSize, playerSize);
+			isKeyPressed = true;
 		}
-		if (Gdx.input.isKeyPressed(Input.Keys.S)){
+		if (Gdx.input.isKeyPressed(Input.Keys.S) && !Gdx.input.isKeyPressed(Input.Keys.W)){
 			player1.testMove(0, -speed);
+			batch.draw(player1.getAnimation().getKeyFrame(timePassed, true),
+			player1.getSprite().getX(), player1.getSprite().getY(),
+			playerSize, playerSize);
+			isKeyPressed = true;
 		}
+
+		/*
+		 * Draw player animation if any key is pressed.
+		 * Did this way so overlapping animations can be avoided.
+		 */
+		if(!isKeyPressed) {
+			batch.draw(player1.getSprite(), player1.getSprite().getX(), player1.getSprite().getY(), playerSize, playerSize);
+		} else {
+			batch.draw(player1.getAnimation().getKeyFrame(timePassed, true),
+			player1.getSprite().getX(), player1.getSprite().getY(),
+			playerSize, playerSize);
+		}
+
+		// Room walkale limits
 		
 		if (Intersector.intersectSegmentRectangle(85, 270, 630, 45, player1.getHitbox())){//this could all be much neater, but it will work for now.
 			player1.testMove(previousX - player1.getSprite().getX(), previousY - player1.getSprite().getY());//moves the player back to previous position
@@ -116,44 +161,6 @@ public class FoodGame extends ApplicationAdapter
 				player1.testMove(1, 0);
 			}
 		}
-		
-		
-		
-		
-		
-		batch.begin();
-		ScreenUtils.clear(0, 0, 0, 0);
-		kitchen1.render(batch);
-		batch.draw(player1.getSprite(), player1.getSprite().getX(), player1.getSprite().getY(), playerSize, playerSize);
-		batch.end();
-		
-		
-		/*
-		//rectangle stuff. remove it all later.
-		shapeRenderer.setProjectionMatrix(batch.getProjectionMatrix());
-		shapeRenderer.setTransformMatrix(batch.getTransformMatrix());
-		player1.renderHitbox(shapeRenderer);
-		
-		//stuff for working out where the lines are
-		shapeRenderer.begin(ShapeType.Line);
-		shapeRenderer.setColor(1, 0, 0, 1);
-		//shapeRenderer.line(85, 270, 1230, 270);
-		//shapeRenderer.line(685, 0, 685, 720);
-		//shapeRenderer.line(0, 45, 1280, 45);
-		shapeRenderer.line(85, 270, 630, 45);//bottom left
-		shapeRenderer.line(1230, 290, 630, 45);//bottom right
-		shapeRenderer.line(85, 460, 685, 700);//top left
-		shapeRenderer.line(1230, 480, 685, 700);//top right
-		shapeRenderer.end();
-		*/
-
 	}
-	@Override
-	public void dispose()
-	{
-		batch.dispose();
-		//shapeRenderer.dispose();//remove this later
-	}
-
 
 }
--- core/src/com/mygdx/game/physics/Player.java
+++ core/src/com/mygdx/game/physics/Player.java
@@ -6,25 +6,35 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.mygdx.game.misc.Coords;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.Array;
 
 
 //testing stuff for the rectangle goes here
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 public class Player extends DynamicObject {
+
+	// Player variables
     private int score;
 	private Rectangle playerHitbox;
-	private Sprite playerSprite;
-	private Texture playerTexture;
 	private boolean facingRight;
-	
+
+	// Animation
+	private Sprite playerSprite;
+	private SpriteBatch batch; 
+	private TextureAtlas walkingAtlas;
+	private Animation<Sprite> animation;
+	private Texture playerTexture;
 	
 
     public Player(float x, float y, float width, float height)
-    {
+    {	
 		score = 0;
 		playerTexture = new Texture("Chef_Still_Image.png");
 		playerSprite = new Sprite(playerTexture);
@@ -32,8 +42,49 @@ public class Player extends DynamicObject {
 		playerSprite.setPosition(x, y);
 		playerHitbox = new Rectangle(x + width + 22, y, width, height);
 		facingRight = true;
+		
     }
+
+	public void create() {
+
+		batch = new SpriteBatch();
+		walkingAtlas = new TextureAtlas(Gdx.files.internal("Cheff_Moving/cheff_spritesheet.atlas"));
+
+		float playerWidth = playerSprite.getWidth();
+		float playerHeight = playerSprite.getHeight();
+
+		animation = new Animation<Sprite>(
+			1/15f,
+			walkingAtlas.createSprite("Left1"),
+			walkingAtlas.createSprite("Left2"),
+			walkingAtlas.createSprite("Left3"),
+			walkingAtlas.createSprite("Left4"),
+			walkingAtlas.createSprite("Left5"),
+			walkingAtlas.createSprite("Left6"),
+			walkingAtlas.createSprite("Left6"),
+			walkingAtlas.createSprite("Left7"));
+			
+		flipAnimation();	
+	}
+
+	public void flipAnimation() {
+		for (TextureRegion frame : animation.getKeyFrames()) {
+			frame.flip(true, false);
+		}
+	}
+
+	public void dispose() {
+		walkingAtlas.dispose();
+	}
 	
+	public Animation<Sprite> getAnimation() {
+		return animation;
+	}
+	public TextureAtlas getAtlas() {
+		return walkingAtlas;
+	}
+
+	// Roddy's classes
 	public Sprite getSprite()
 	{
 		return playerSprite;
@@ -41,16 +92,16 @@ public class Player extends DynamicObject {
 	
 	public Rectangle getHitbox()
 	{
-			return playerHitbox;
+		return playerHitbox;
 	}
 	
 	public boolean getFace()
 	{
-			return facingRight;
+		return facingRight;
 	}
 	public void setFace(boolean newDirection)
 	{
-			facingRight = newDirection;
+		facingRight = newDirection;
 	}
 	
 	
