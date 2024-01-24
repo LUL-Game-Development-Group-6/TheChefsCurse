af2b710eec2e97700c77c651154673efa52e1403
Juozas Skarbalius <j.skarbalius@lancaster.ac.uk>
2024-01-24 16:59:39 +0100

    GD-97: render (show) generated enemies progressively

--- core/src/com/mygdx/game/physics/EnemiesGenerator.java
+++ core/src/com/mygdx/game/physics/EnemiesGenerator.java
@@ -30,10 +30,63 @@ public class EnemiesGenerator {
         }
     }
 
-    public void renderAllEnemies(float renderTime, Vector2 playerPosition, SpriteBatch batch, float playerSize) {
-        for(Enemy enemy : factory.getEnemies()) {
+    public void renderAllEnemies(float renderTime, Vector2 playerPosition, SpriteBatch batch, float playerSize,
+                                 float delta) {
+        // every 15 seconds
+        if(timeElapsedSinceLastSpawn >= 5 && enemiesSpawned <= MAX_ENEMY_POOL_SIZE) {
+            // spawn random batch of enemies
+            int enemiesToSpawn = generateNextRandomChuckSize();
+            System.out.println("Spawning... === " + enemiesToSpawn + " ...... " + enemiesSpawned + " ---- " + recentlySpawnedEnemies.size());
+            int diff = enemiesToSpawn + enemiesSpawned;
+            for(int i = enemiesSpawned - 1; i < diff; i++) {
+                try {
+                    Enemy enemy = factory.getEnemies().get(i);
+                    renderEnemy(enemy, batch, renderTime, playerPosition, playerSize);
+                } catch (NullPointerException | ArrayIndexOutOfBoundsException e) {
+                    System.out.println("[WARN] Unable to spawn enemy. " + e);
+                }
+                enemiesSpawned++;
+            }
+
+            // reset time tracker
+            timeElapsedSinceLastSpawn = 0;
+        } else if(enemiesSpawned == 0){
+            try {
+                Enemy enemy = factory.getEnemies().get(0);
+                renderEnemy(enemy, batch, renderTime, playerPosition, playerSize);
+            } catch (NullPointerException | ArrayIndexOutOfBoundsException e) {
+                System.out.println("[WARN] Unable to spawn enemy. " + e);
+            }
+            timeElapsedSinceLastSpawn = 0;
+            enemiesSpawned++;
+        } else {
+            timeElapsedSinceLastSpawn += delta;
+        }
+
+        // render all drawn enemies
+        renderRecentlySpawnedEnemies(batch, renderTime, playerPosition, playerSize);
+    }
+
+    private void renderEnemy(Enemy enemy, SpriteBatch batch, float renderTime, Vector2 playerPosition,
+                             float playerSize) {
+        System.out.println("adding + " + recentlySpawnedEnemies.size());
+        recentlySpawnedEnemies.add(enemy);
+    }
+
+    private void renderRecentlySpawnedEnemies(SpriteBatch batch, float renderTime, Vector2 playerPosition,
+                                              float playerSize) {
+        for(Enemy enemy : recentlySpawnedEnemies) {
             batch.draw(enemy.getSprite(), enemy.getSprite().getX(), enemy.getSprite().getY(), playerSize, playerSize);
             enemy.render(renderTime, playerPosition);
         }
+    }
+
+    private int generateNextRandomChuckSize() {
+        if(MAX_ENEMY_POOL_SIZE - enemiesSpawned > 0) {
+            int maxBound = MAX_ENEMY_POOL_SIZE - enemiesSpawned;
+            int minBound = 1;
+            return ThreadLocalRandom.current().nextInt(minBound, maxBound);
+        }
+        return -1;
     }
 }
