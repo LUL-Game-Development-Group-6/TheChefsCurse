190fe3adcf08f6d99e21d210703467f14af414c5
terahidro2003 <juozas2003@gmail.com>
2024-01-29 19:10:38 +0200

    GD-101: add missing `Sprite` lib

--- core/src/com/mygdx/game/FoodGame.java
+++ core/src/com/mygdx/game/FoodGame.java
@@ -1,5 +1,6 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
