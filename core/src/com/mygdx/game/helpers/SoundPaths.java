1fe7dd4f6772ee74ffe8764c7e7742e85224f628
mccarronr <122781875+mccarronr@users.noreply.github.com>
2024-02-15 13:23:46 +0100

    Sound file paths.
    I put them all in one file so it is easier to keep track of

---
+++ core/src/com/mygdx/game/helpers/SoundPaths.java
@@ -0,0 +1,16 @@
+package com.mygdx.game.helpers;
+
+public class SoundPaths
+{
+	public static final String SHOTGUN_PATH = "sound/shotgunFire.wav";
+	public static final String REDGUN_PATH = "sound/redgunFire.wav";
+	public static final String PLAYERHIT_PATH = "sound/hit.wav";//might add different ones for this. I don't know. Sounds fine as it is
+	public static final String ENEMYHIT_PATH = "sound/hit.wav";
+	public static final String PLAYERDEAD_PATH = "sound/dead.wav";
+	public static final String ENEMYDEAD_PATH = "sound/enemyDies.wav";
+	public static final String ENEMYGUN_PATH = "sound/enemyShoots.wav";
+	public static final String BUTTON_PATH = "sound/buttonPress.wav";
+	public static final String BUYUPGRADE_PATH = "sound/upgrade.wav";
+	
+	public static final String MUSIC_PATH = "sound/music.ogg";//this one uses .ogg because there is a slight delay at the start for other formats and it was making the looping sound bad
+}
