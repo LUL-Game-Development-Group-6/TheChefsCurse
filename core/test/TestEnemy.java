c65adfbb5f4ed9d407b96aecebff2ba63e9191c0
Juozas Skarbalius <j.skarbalius@lancaster.ac.uk>
2024-03-01 15:27:27 +0100

    GD-160: JUnit config

---
+++ core/test/TestEnemy.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestEnemy {
+
+    @Test
+    public void randomizeEnemyDirection() {
+        assertTrue(true);
+
+    }
+}
