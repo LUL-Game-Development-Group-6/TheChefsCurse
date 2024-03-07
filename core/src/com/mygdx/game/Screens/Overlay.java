41fd8dbec762d42583d4a4ed3f709d51cb714937
Juozas Skarbalius <j.skarbalius@lancaster.ac.uk>
2024-03-07 12:06:08 +0100

    GD-160: Refactor `Overlay` class

--- core/src/com/mygdx/game/Screens/Overlay.java
+++ core/src/com/mygdx/game/Screens/Overlay.java
@@ -7,81 +7,62 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.mygdx.game.helpers.Fonts;
-import com.mygdx.game.physics.EnemiesGenerator;
 import com.mygdx.game.physics.Player;
 
+/**
+ * Class for in-game overlay for game metrics and user interaction
+ * elements, and animations
+ * Please refer to {@link com.mygdx.game.Screens.Overlay}
+ * @author Gines Moratalla, Juozas Skarbalius
+ */
 public class Overlay implements Screen {
-    private Texture OverlayTexture;
-    private Sprite OverlaySprite;
-
-    // Textures for standing sprites (3 weapons)
-    private Texture RedGunTexture;
-    private Sprite RedGunSprite;
-
-    private Texture ShotGunTexture;
-    private Sprite ShotGunSprite;
-
-    private Texture HandTexture;
-    private Sprite HandSprite;
-
-    // Enemies left
-    private Texture enemiesLeftTexture;
-	private Sprite enemiesLeftSprite;
-
-
-	private Texture overlayTexture;
-	private Sprite overlaySprite;
-
-    private BitmapFont font;
-    private BitmapFont roundFont;
-    private BitmapFont roundNumber;
-    private Fonts myFont;
-
-    private TextButtonStyle nextRound;
-    private TextButton nextRound_button;
-    private Stage stage;
-    private FoodGame foodGame;
-    private Menu game;
-
-    private Music gameMusic;
-    
-
+    private final Sprite OverlaySprite;
+    private final Sprite RedGunSprite;
+    private final Sprite ShotGunSprite;
+    private final Sprite HandSprite;
+    private final Sprite enemiesLeftSprite;
+    private final Sprite overlaySprite;
+    private final BitmapFont font;
+    private final BitmapFont roundFont;
+    private final BitmapFont roundNumber;
+    private final TextButton nextRound_button;
+    private final Stage stage;
+    private final FoodGame foodGame;
+    private final Menu game;
+    private final Music gameMusic;
     SpriteBatch batch;
 
     public Overlay(FoodGame foodGame, Music gameMusic) {
         this.gameMusic = gameMusic;
         this.foodGame = foodGame;
         this.game = Menu.getInstance();
-        myFont = new Fonts();
+        Fonts myFont = new Fonts();
         batch = new SpriteBatch();
 
         // Overlay
-		OverlayTexture = new Texture("cheff/Weapon_Overlay.png");
-		OverlaySprite = new Sprite(OverlayTexture);
+        Texture overlayTexture1 = new Texture("cheff/Weapon_Overlay.png");
+		OverlaySprite = new Sprite(overlayTexture1);
 
-		RedGunTexture = new Texture("cheff/weapons/RedGun.png");
-		RedGunSprite = new Sprite(RedGunTexture);
-		
-		ShotGunTexture = new Texture("cheff/weapons/Shotgun.png");
-		ShotGunSprite = new Sprite(ShotGunTexture);
+        Texture redGunTexture = new Texture("cheff/weapons/RedGun.png");
+		RedGunSprite = new Sprite(redGunTexture);
 
-		HandTexture = new Texture("cheff/weapons/Hand.png");
-		HandSprite = new Sprite(HandTexture);
+        Texture shotGunTexture = new Texture("cheff/weapons/Shotgun.png");
+		ShotGunSprite = new Sprite(shotGunTexture);
+
+        Texture handTexture = new Texture("cheff/weapons/Hand.png");
+		HandSprite = new Sprite(handTexture);
 
 
         // Panel that showcases the enemies left
-        overlayTexture = new Texture("buttons/Rectangle_enemies_left.png");
+        Texture overlayTexture = new Texture("buttons/Rectangle_enemies_left.png");
 		overlaySprite = new Sprite(overlayTexture);
 
-		enemiesLeftTexture = new Texture("buttons/Enemies_left_text.png");
+        Texture enemiesLeftTexture = new Texture("buttons/Enemies_left_text.png");
 		enemiesLeftSprite = new Sprite(enemiesLeftTexture);
 
         // Handle font
@@ -91,14 +72,8 @@ public class Overlay implements Screen {
 
         // Next round button
         stage = new Stage();
-        nextRound = new TextButtonStyle();
-        nextRound.up = new TextureRegionDrawable(new TextureRegion(new Texture("buttons/next_round_NoClick.png")));
-        nextRound.down = new TextureRegionDrawable(new TextureRegion(new Texture("buttons/next_round_Clicked.png")));
-        nextRound.over = new TextureRegionDrawable(new TextureRegion(new Texture("buttons/next_round_Hover.png")));
-        nextRound.font = font;
-        nextRound_button = new TextButton("", nextRound);
+        nextRound_button = Cover.createButton("buttons/next_round_NoClick.png", "buttons/next_round_Clicked.png", "buttons/next_round_Hover.png", font, 500, 30);
         nextRound_button.setSize(nextRound_button.getWidth()/2, nextRound_button.getHeight()/2);
-        nextRound_button.setPosition(500, 30);
         stage.addActor(nextRound_button);
         show();
 
@@ -117,13 +92,13 @@ public class Overlay implements Screen {
         });
     }
 
-    public void render(Player player1, int enemiesLeft, EnemiesGenerator generator, int counter) {
+    public void render(Player player1, int counter) {
 
         batch.begin();
         // Render player's health and overlay at the top left of the screen
 
         batch.draw(OverlaySprite, 0, 550, OverlaySprite.getWidth()/2, OverlaySprite.getHeight()/2);
-        batch.draw(player1.getHealthSprite(), 155, 643, player1.getHealthSprite().getWidth()/2 - 20, player1.getHealthSprite().getHeight()/2 - 10);
+        batch.draw(player1.getHealthSprite(), 155, 643, (float) player1.getHealthSprite().getWidth() /2 - 20, (float) player1.getHealthSprite().getHeight() /2 - 10);
 		player1.healthPercentage();
 
 
@@ -139,15 +114,11 @@ public class Overlay implements Screen {
 			batch.draw(ShotGunSprite, 22, 600, ShotGunSprite.getWidth()/3, ShotGunSprite.getHeight()/3);	
 		}
 
-
         // Render things related to the overlay
 		batch.draw(overlaySprite, 845, 580, overlaySprite.getWidth()/2 - 25, overlaySprite.getHeight()/2 - 10);
 		batch.draw(enemiesLeftSprite, 870, 620, enemiesLeftSprite.getWidth()/3f, enemiesLeftSprite.getHeight()/3f);
 
-
-
         String enemiesLeftStr = Integer.toString(counter);
-        //System.out.println(counter);
 
 		font.draw(batch, enemiesLeftStr,centerX(enemiesLeftStr),  650);
         roundFont.draw(batch, "xp:",30,  150);
@@ -161,18 +132,16 @@ public class Overlay implements Screen {
     }
 
     public void nextRound() {
-        
         stage.act();
         Gdx.input.setInputProcessor(stage);
         stage.draw();
     }
 
-    /*
-     * This only returns the x coordinate depending on the ammount of enemies
-     * to center the numbers
+    /**
+     * <p>This only returns the x coordinate depending on the ammount of enemies
+     * to center the numbers</p>
      */
     private int centerX(String numberString) {
-
         if(numberString.length() == 1) {
             return 1160;
         } else if (numberString.length() == 2) {
@@ -187,7 +156,6 @@ public class Overlay implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
     }
 
     @Override
